33860a86eefaec628817ffdd62996c0739350d0b
Cameron <cameron_griego@protonmail.com>
2026-05-04 04:52:29 -0600

    Chore; Move build tools

--- build-tools/crates/infinity-build-core/src/builder.rs
+++
@@ -1,8 +0,0 @@
-use crate::{Artifact, BuildResult};
-
-pub trait Builder {
-    type Input;
-    type Output: Artifact;
-
-    fn build(&self, input: &Self::Input) -> BuildResult<Self::Output>;
-}
--- build-tools/crates/infinity-build-core/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod artifact;
-pub mod builder;
-pub mod error;
-
-pub use artifact::{Artifact, FileKind, GeneratedFile, SimpleArtifact, pick_primary, stat_file};
-pub use builder::Builder;
-pub use error::{BuildError, BuildResult};
